0d7c7bdc29086b9d8e7931c48799ddd5acdde642
Charlton Smith <charltonsmith@hotmail.com>
2016-04-10 19:49:24 -0700

    Added files via upload

---
+++ Clip/src/Converter.java
@@ -0,0 +1,9 @@
+
+
+/**
+ * This class is responsible for converting a large video file (e.x. AVI) to a smaller,
+ * more web friendly video file format (e.x. WebM).
+ */
+public class Converter {
+	
+}
